e11c7a19b7daff42670693a736fa268ef1a210f4
Rohit Jain <rohit@Elitebook-Server.eb.local>
2020-09-09 13:50:39 +0530

    added custom error messages while ommiting error

--- scripts/check.py
+++ scripts/check.py
@@ -3,25 +3,37 @@ import sys
 import json
 
 parent_dir = "./asset/"
+SYMBOL_NOT_PRESENT_ERROR = "json does not have 'symbol' key"
+NAME_NOT_PRESENT_ERROR = "json does not have 'name' key"
+CAPIP20_NOT_PRESENT_ERROR = "json does not have 'capip-20' key"
+IOV_NAME_SERVICE_URI_NOT_PRESENT_ERROR = "json does not have 'iov-name-service-uri' key"
+SYMBOL_FILENAME_MATCH_ERROR = "asset name does not match with filename. Name the file as {assetname}.json"
+IOV_NAME_SERVICE_URI_DOES_NOT_MATCH_ERROR = "'iov-name-service-uri' should be in the form 'asset:{assetname}'"
 
-def error():
+def error(error_message, d):
+  print(error_message)
+  print(json.dumps(d, indent=4))
   sys.exit(1)
 
 def check_format(d):
-  if "symbol" not in d or "name" not in d or "caip-20" not in d or "iov-name-service-uri" not in d:
-    print('incorrect json')
-    error()
+  if "symbol" not in d:
+    error(SYMBOL_NOT_PRESENT_ERROR, d)
+  if "name" not in d:
+    error(NAME_NOT_PRESENT_ERROR, d)
+  if "caip-20" not in d:
+    error(CAPIP20_NOT_PRESENT_ERROR, d)
+  if "iov-name-service-uri" not in d:
+    error(IOV_NAME_SERVICE_URI_NOT_PRESENT_ERROR, d)
 
 for file in os.listdir(parent_dir):
-  # print(file)
   with open(os.path.join(parent_dir, file), "r") as f:
     d = json.load(f)
     check_format(d)
     asset = file[:-5]
     if d['symbol'].lower() != asset:
-      error()
+      error(SYMBOL_FILENAME_MATCH_ERROR, d)
     if d['iov-name-service-uri'] != ('asset:'+asset):
-      error()
+      error(IOV_NAME_SERVICE_URI_DOES_NOT_MATCH_ERROR, d)
 
 sys.exit()
 
