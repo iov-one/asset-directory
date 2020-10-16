e36b06a0fdec25bb87263e52cf30948cf6803abb
Dave Puchyr <dave@slim.avaritia.com>
2020-10-16 11:31:02 +0200

    Aggregate on both asset and metadata

--- aggregate.js
+++ aggregate.js
@@ -2,14 +2,22 @@ const fs = require( "fs" );
 const path = require( "path" );
 const stringify = require( "json-stable-stringify" );
 
+"use strict";
 
 const assets = [];
 
-fs.readdirSync( "assets" ).forEach( file => {
-   const json = fs.readFileSync( path.join( "assets", file ), "utf-8" );
-   const o = JSON.parse( json );
+process.chdir( path.join( ".", "assets" ) );
+fs.readdirSync( "." ).filter( dir => fs.statSync( dir ).isDirectory() ).forEach( dir => {
+   const fileAsset = path.join( dir, "asset.json" ); // HARD-CODED
+   const fileMetadata = path.join( dir, "metadata", "info.json" ); // HARD-CODED
+   const jsonAsset = fs.readFileSync( fileAsset, "utf-8" );
+   const jsonMetadata = fs.readFileSync( fileMetadata, "utf-8" );
+   const asset = JSON.parse( jsonAsset );
+   const metadata = JSON.parse( jsonMetadata );
 
-   assets.push( o );
+   Object.keys( metadata ).forEach( key => asset[key] = metadata[key] );
+
+   assets.push( asset );
 } );
 
 fs.writeFileSync( "assets.json", stringify( assets.sort( ( a, b ) => a.symbol.localeCompare( b.symbol ) ), { space: "  " } ) + "\n" );
