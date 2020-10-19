74d01cdd5ee5ebc4c1e05b89e708410eb52c78d3
Dave Puchyr <dave@slim.avaritia.com>
2020-10-19 17:41:02 +0200

    Make logo data:image/png;base64

--- aggregate.js
+++ aggregate.js
@@ -1,12 +1,12 @@
 const fs = require( "fs" );
+const encoding = require( "@cosmjs/encoding" );
 const path = require( "path" );
 const stringify = require( "json-stable-stringify" );
 
 "use strict";
 
-const assets = [];
 
-fs.readdirSync( "assets" ).filter( dir => fs.statSync( path.join( "assets", dir ) ).isDirectory() ).forEach( dir => {
+const assets = fs.readdirSync( "assets" ).filter( dir => fs.statSync( path.join( "assets", dir ) ).isDirectory() ).map( dir => {
    const fileAsset = path.join( "assets", dir, "asset.json" ); // HARD-CODED
    const fileMetadata = path.join( "assets", dir, "metadata", "info.json" ); // HARD-CODED
    const jsonAsset = fs.readFileSync( fileAsset, "utf-8" );
@@ -30,10 +30,16 @@ fs.readdirSync( "assets" ).filter( dir => fs.statSync( path.join( "assets", dir 
       asset.name = info.name;
    }
 
-   // possibly inject logo
-   if ( !asset.logo && metadata["trustwallet-info"] ) asset.logo = metadata["trustwallet-info"].replace( "info.json", "logo.png" ); // HARD-CODED
+   // replace logo with base64
+   try { // dmjp
+   const fileLogo = asset.logo || path.join( ".", metadata["trustwallet-info"].replace( "info.json", "logo.png" ) ); // HARD-CODED
+   const binary = fs.readFileSync( fileLogo );
+   const logo = encoding.toBase64( binary );
 
-   assets.push( asset );
+   asset.logo = `data:image/png;base64,${logo}`;
+   } catch (e ) {}
+
+   return asset;
 } );
 
 fs.writeFileSync( "assets.json", stringify( assets.sort( ( a, b ) => a.symbol.localeCompare( b.symbol ) ), { space: "  " } ) + "\n" );
