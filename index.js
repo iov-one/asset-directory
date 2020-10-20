576df8cd01a9c836596065ab50e39aafe516bc17
Dave Puchyr <dave@slim.avaritia.com>
2020-10-20 19:29:58 +0200

    Export ./starname/assets.json

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-exports.assets = JSON.parse( require( "fs" ).readFileSync( "assets.json", "utf-8" ) );
+const fs = require( "fs" );
+const path = require( "path" );
+
+"use strict";
+
+exports.assets = JSON.parse( fs.readFileSync( "assets.json", "utf-8" ) ); // HARD-CODED
+
+exports.assetsStarname = JSON.parse( fs.readFileSync( path.join( "starname", "assets.json" ), "utf-8" ) ); // HARD-CODED
