23fbf66302b3d6028247a40c4ef5594554b397da
Rohit Jain <rohitjain18005@gmail.com>
2020-10-22 19:54:16 +0530

    files formatted

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-const fs = require( "fs" );
-const path = require( "path" );
+const fs = require("fs");
+const path = require("path");
 
-"use strict";
+("use strict");
 
-exports.assets = JSON.parse( fs.readFileSync( "assets.json", "utf-8" ) ); // HARD-CODED
+exports.assets = JSON.parse(fs.readFileSync("assets.json", "utf-8")); // HARD-CODED
 
-exports.assetsStarname = JSON.parse( fs.readFileSync( path.join( "starname", "assets.json" ), "utf-8" ) ); // HARD-CODED
+exports.assetsStarname = JSON.parse(
+  fs.readFileSync(path.join("starname", "assets.json"), "utf-8"),
+); // HARD-CODED
--- unique.js
+++ unique.js
@@ -1,29 +1,39 @@
-const fetch = require( "node-fetch" );
+const fetch = require("node-fetch");
 
-"use strict";
-
+("use strict");
 
 const main = async () => {
-   const fetched = await fetch( "https://raw.githubusercontent.com/trustwallet/wallet-core/master/coins.json" ).catch( e => { throw e } );
-   const trust = await fetched.json().catch( e => { throw e } );
-   const coins = {};
+  const fetched = await fetch(
+    "https://raw.githubusercontent.com/trustwallet/wallet-core/master/coins.json",
+  ).catch((e) => {
+    throw e;
+  });
+  const trust = await fetched.json().catch((e) => {
+    throw e;
+  });
+  const coins = {};
 
-   trust.forEach( coin => {
-      const symbol = coin.symbol;
-      const seen = coins.hasOwnProperty( symbol );
+  trust.forEach((coin) => {
+    const symbol = coin.symbol;
+    const seen = coins.hasOwnProperty(symbol);
 
-      if ( !seen ) coins[symbol] = [];
+    if (!seen) coins[symbol] = [];
 
-      coins[symbol].push( coin.coinId );
-   } );
+    coins[symbol].push(coin.coinId);
+  });
 
-   Object.keys( coins ).forEach( symbol => coins[symbol].length > 1 && console.warn( `${symbol} has ids ${coins[symbol].join( ", " )}.` ) );
-}
+  Object.keys(coins).forEach(
+    (symbol) =>
+      coins[symbol].length > 1 &&
+      console.warn(`${symbol} has ids ${coins[symbol].join(", ")}.`),
+  );
+};
 
-
-main().then( () => {
-   process.exit( 0 );
-} ).catch( e => {
-   console.error( e.stack || e.message || e );
-   process.exit( -1 );
-} );
+main()
+  .then(() => {
+    process.exit(0);
+  })
+  .catch((e) => {
+    console.error(e.stack || e.message || e);
+    process.exit(-1);
+  });
