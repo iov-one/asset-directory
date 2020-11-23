f3085d888ea0037176170201a70e85080233927f
davepuchyr <dave.puchyr@avaritia.com>
2020-11-23 10:23:07 +0100

    Add CELO (#23)
    * Update trustwallet assets
    
    * Add CELO WIP
    
    * Fix caip-19 and trustwallet-uid
    
    * Bump version
    
    Co-authored-by: Dave Puchyr <dave@slim.avaritia.com>

--- assets.js
+++ assets.js
@@ -40,6 +40,11 @@ export default [
     "trustwallet-uid": "c0"
   },
   {
+    "caip-19": null,
+    "symbol": "CELO",
+    "trustwallet-uid": "c52752"
+  },
+  {
     "caip-19": "eip155:1/erc20:0xc00e94cb662c3520282e6f5717214004a7f26888",
     "symbol": "COMP",
     "trustwallet-uid": "c60_t0xc00e94cb662c3520282e6f5717214004a7f26888"
