e442bf92c1bce9102c51ee5a1b32c758f09b273f
Dave Puchyr <dave@slim.avaritia.com>
2020-12-07 14:51:37 +0100

    Add wIOV

--- assets.js
+++ assets.js
@@ -155,6 +155,11 @@ export default [
     "trustwallet-uid": "c60_t0x2260fac5e5542a773aa44fbcfedf7c193bc2c599"
   },
   {
+    "caip-19": "eip155:1/erc20:0x761d827e90b1bd013f57289ed36e68ef42ef6f33",
+    "symbol": "wIOV",
+    "trustwallet-uid": null
+  },
+  {
     "caip-19": null,
     "symbol": "XLM",
     "trustwallet-uid": "c148"
